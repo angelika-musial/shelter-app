8f8f65f4e64efb79d9f68c86056e8136a1bd2e10
Angelika Musiał <angelikamusial@tlen.pl>
2026-02-14 13:23:20 +0100

    feat: render animals list with dynamic mapping

--- frontend/src/components/animals/AnimalList.tsx
+++ frontend/src/components/animals/AnimalList.tsx
@@ -1,5 +1,18 @@
-const AnimalList = () => {
-	return <h1>Animal List</h1>;
+import AnimalCard from './AnimalCard';
+import type { Animal } from '../../types/animal.types';
+
+interface AnimalsProps {
+	animals: Animal[];
+}
+
+const AnimalList = ({ animals }: AnimalsProps) => {
+	return (
+		<div className='grid md:grid-cols-3 gap-6'>
+			{animals.map((animal) => (
+				<AnimalCard key={animal._id} animal={animal} />
+			))}
+		</div>
+	);
 };
 
 export default AnimalList;
