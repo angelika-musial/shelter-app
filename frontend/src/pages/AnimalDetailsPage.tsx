29a6ef7d685dbba8b32d8e5fd9f1344489c894ab
Angelika Musiał <angelikamusial@tlen.pl>
2026-02-14 13:26:17 +0100

    feat: implement animal details page with data fetching and basic styling

--- frontend/src/pages/AnimalDetailsPage.tsx
+++ frontend/src/pages/AnimalDetailsPage.tsx
@@ -1,6 +1,40 @@
+import type { Animal } from '../types/animal.types';
+import { useEffect, useState } from 'react';
+import { getAnimalById } from '../api/animals.api';
+import { useParams } from 'react-router-dom';
+
 const AnimalDetailsPage = () => {
+	const { id } = useParams();
+	const [animal, setAnimal] = useState<Animal | null>(null);
+
+	useEffect(() => {
+		const fetchAnimal = async () => {
+			if (id) {
+				const response = await getAnimalById(id);
+				setAnimal(response.data);
+			}
+		};
+		fetchAnimal();
+	}, [id]);
+
+	if (!animal) return <p>Loading...</p>;
+
 	return (
-		<h1 className='text-4xl font-bold text-center py-10'>Animal Details</h1>
+		<div className='max-w-4xl mx-auto p-6'>
+			<img
+				src={animal.imageUrl}
+				alt={animal.name}
+				className='h-96 w-fit object-cover rounded-md'
+			/>
+
+			<h1 className='text-3xl font-bold mt-6'>{animal.name}</h1>
+
+			<p className='mt-2 text-gray-600'>
+				{animal.species} • {animal.breed} • {animal.age} years old
+			</p>
+
+			<p className='mt-4'>{animal.description}</p>
+		</div>
 	);
 };
 
