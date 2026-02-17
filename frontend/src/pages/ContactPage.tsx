7e85dbf71d16bb1930fd5228ac27cce425be9243
Angelika Musiał <angelikamusial@tlen.pl>
2026-02-17 22:17:05 +0100

    feat: create ContactPage with ContactForm component

--- frontend/src/pages/ContactPage.tsx
+++ frontend/src/pages/ContactPage.tsx
@@ -1,5 +1,18 @@
+import ContactForm from '../components/contact/ContactForm';
+
 const ContactPage = () => {
-	return <h1 className='text-4xl font-bold text-center'>Contact Page</h1>;
+	return (
+		<div className='max-w-3xl mx-auto p-2'>
+			<h1 className='text-3xl font-bold mb-4'>Contact Us</h1>
+
+			<p className='text-gray-600 mb-8'>
+				Have a question about adoption or our shelter? Send us a message and
+				we’ll get back to you.
+			</p>
+
+			<ContactForm />
+		</div>
+	);
 };
 
 export default ContactPage;
